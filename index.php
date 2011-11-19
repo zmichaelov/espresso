9f890f445e1f10569692c142d6a732b1eb5e5820
Zachary Michaelov <zach.michaelov@gmail.com>
2011-11-19 02:52:10 -0500

    Added stylesheets and AJAX output
    Added Bootstrap from Twitter for styles. Added JQuery for AJAX and DOM
    manipulation

--- index.php
+++ index.php
@@ -2,47 +2,51 @@
 <html>
 	<head>
 		<title>Espresso Web App</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">		
 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-		<script type="text/javascript">
-			$(document).ready(function(){
-				//generateTable();
-			});
-			function int2bin(number, padding) {
-				var temp = number.toString(2);
-				while(temp.length < padding) {
-					temp = "0" + temp;							
-				}
-				return temp;
-			}
-			function generateTable() {
-				$("#char_table").empty();
-				// get number of input variables
-				var inputs = $("#inputs").val();
-				// calculate number of min terms
-				var minterms = Math.pow(2, inputs);
-				var options = "<select><option>0</option><option>1</option><option>D</option></select>";
-				for(var i = 0; i < minterms; i++) {
-					$("#char_table").append("<tr id="+i+"><td>"+i+"</td>");
- 					var binary = int2bin(i, inputs);
- 					$("#"+i).append('<td>'+binary+'</td>');
- 					//$("#"+i).append('<td><input type="text"/></td>');
- 					$("#"+i).append('<td>'+options+'</td>');
-					$("#char_table").append("</tr>");
-				}
-
-			}
-		</script>
+		<script type="text/javascript" src="js/espresso.js"></script>
 	</head>
 	<body>
-		<form action="utils/espresso.php" id="espresso" method="post">
-			<p>Number of input variables:</p><input type="text" name="inputs" id="inputs" value=""/>
-			<p>Minterms (separated by spaces):</p><input type="text" name="minterms" id="minterms" value=""/>
-			<p>Don't Cares (leave blank if none):</p><input type="text" name="dontcares" id="dontcares" value=""/>
-			<input type="submit" value="Submit" />
-		</form>			
-		<div id="main">
-			<table id="char_table"></table>
-		</div>
+		<div class="topbar">
+		  <div class="fill">
+			<div class="container">
+			  <a class="brand" href="index.php">Espresso</a>
+			  <ul class="nav">
+				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="#about">About</a></li><!--Make these popouts-->
+				<li><a href="#contact">Contact</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</div><!-- topbar-->
+	<div class="container">
+      <div class="content">
+        <div class="page-header">
+          <h2>Espresso<small> the web-based logic minimizer</small></h2>
+        </div>
+        <div class="row">
+        	<h3>Inputs</h3>
+			<div class="span10">
+				<form action="" id="inputForm" method="post">
+					<p>Number of input variables:</p><input type="text" name="inputs" value=""/>
+					<p>Minterms (separated by spaces):</p><input type="text" name="minterms" value=""/>
+					<p>Don't Cares (leave blank if none):</p><input type="text" name="dontcares" value=""/>
+					<input type="submit" value="Submit" />
+				</form>
+			  </div>
+        </div>
+        <div class="row">
+			<h3>Native Output</h3>
+				<div class="span10" id="output"></div>
+        </div>
+      </div>
+
+      <footer>
+        <p>&copy; Zach Michaelov 2011</p>
+      </footer>
+
+    </div> <!--container -->
 	</body>
 </html>
 <!-- 
