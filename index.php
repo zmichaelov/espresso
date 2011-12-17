d4cd574a95a679686230dc04839888d0ba9840b6
Zachary Michaelov <zach.michaelov@gmail.com>
2011-12-17 17:32:56 -0500

    Removed top nav bar. Added about modal popup

--- index.php
+++ index.php
@@ -9,88 +9,111 @@
 		<script type="text/javascript" src="js/espresso.js"></script>
 		<script type="text/javascript" src="js/prettify.js"></script>
 		<script type="text/javascript" src="js/lang-tex.js"></script>
+		<script type="text/javascript" src="js/bootstrap-modal.js"></script>
 	</head>
 	<body onload="prettyPrint()">
 		<div class="topbar">
-		  <div class="fill">
+<!-- 
+			<div class="fill">
 			<div class="container">
 			  <a class="brand" href="index.php">Espresso</a>
 			  <ul class="nav">
-				<li class="active"><a href="index.php">Home</a></li>
-				<li><a href="#about">About</a></li>
+				<li><a href="#" data-controls-modal="about-modal" data-backdrop="true" >About</a></li>
 				<li><a href="#contact">Contact</a></li>
 			  </ul>
 			</div>
-		  </div>
-		</div><!-- topbar-->
-	<a href="https://github.com/zmichaelov/espresso"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub"></a>
-	<div class="container">
-      <div class="content">
-        <div class="page-header">
-          <h2>Espresso<small> Your web-based logic minimizer</small></h2>
-        </div>
-        <div class="row">
-
-			<div class="span8">
-				<h3>Inputs</h3>
-				<form action="" id="inputForm" method="post">
-					
-					<div class="clearfix" id="variables">
-						<label for="variables"># of variables</label>
-						<div class="input" id="variables">
-							<input id="variables" type="text" value=""/>
-							<span class="help-inline" id="variables"></span>
-						</div>
-					</div>
-					
-					<div class="clearfix" id="clearfix_minterms">
-						<label for="minterms">Minterms</label>
-						<div class="input">
-							<input id="minterms" type="text" value=""/>
-							<span class="help-inline" id="minterms_help"></span>
-						</div>
-					</div>
-					<div class="clearfix" id="clearfix_dontcares">
-						<label for="dontcares">Don't Cares</label>
-						<div class="input">
-							<input id="dontcares" type="text" value=""/>
-							<span class="help-inline"></span>
-						</div>
-					</div>					
-					<div class="row">
-						<div class="span4 offset4">
-							<input class="btn primary" type="submit" value="Submit" />
-							<button class="btn" type="reset">Reset</button>
-						</div>
-					</div>
-				</form>
-			  </div>
-<!-- 
-			  <div class="span8">
-					<h3>Additional Options</h3>
-			  </div>
+			</div>
  -->
-        </div>
-        <div class="row">
-			<div class="span8"><h3>Standard Output</h3>
-				<div id="output"></div>
+ 
+		<a href="https://github.com/zmichaelov/espresso"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub"></a>
+		</div><!-- topbar-->
+		<div class="container-fluid">
+		  <div class="content">		  
+			<div class="page-header">
+				<h2>Espresso<small> Your web-based logic minimizer</small></h2>
+				<small><a href="#" data-controls-modal="about-modal" data-backdrop="true" >About</a></small>
 			</div>
-			<div class="span8"><h3>Latex</h3>
-				<div id="latex_rendered"></div>
+			<div class="row">
+	
+				<div class="span8">
+					<h3>Inputs</h3>
+					<form action="" id="inputForm" method="post">
+						
+						<div class="clearfix" id="variables">
+							<label for="variables"># of variables</label>
+							<div class="input" id="variables">
+								<input id="variables" type="text" value=""/>
+								<span class="help-inline" id="variables"></span>
+							</div>
+						</div>
+						
+						<div class="clearfix" id="clearfix_minterms">
+							<label for="minterms">Minterms</label>
+							<div class="input">
+								<input id="minterms" type="text" value=""/>
+								<span class="help-inline" id="minterms_help"></span>
+							</div>
+						</div>
+						<div class="clearfix" id="clearfix_dontcares">
+							<label for="dontcares">Don't Cares</label>
+							<div class="input">
+								<input id="dontcares" type="text" value=""/>
+								<span class="help-inline"></span>
+							</div>
+						</div>					
+						<div class="row">
+							<div class="span4 offset4">
+								<input class="btn primary" type="submit" value="Submit" />
+								<button class="btn" type="reset">Reset</button>
+							</div>
+						</div>
+					</form>
+				  </div>
+	<!-- 
+				  <div class="span8">
+						<h3>Additional Options</h3>
+				  </div>
+	 -->
 			</div>
-        </div>
-		<div class="row">
-			<div class="span16"><h3>Console</h3>
-				<textarea class="xxlarge" id="console"></textarea>
+			<div class="row">
+				<div class="span8"><h3>Standard Output</h3>
+					<div id="output"></div>
+				</div>
+				<div class="span8"><h3>Latex</h3>
+					<div id="latex_rendered"></div>
+				</div>
 			</div>
-		</div>
-      </div>
+			<div class="row">
+				<div class="span16"><h3>Console</h3>
+					<textarea class="xxlarge" id="console"></textarea>
+				</div>
+			</div>
+		  </div>
+	
+		  <footer>
+			<p>&copy; Zach Michaelov 2011</p>
+		  </footer>
+	
+		</div> <!--container -->
 
-      <footer>
-        <p>&copy; Zach Michaelov 2011</p>
-      </footer>
-
-    </div> <!--container -->
+		<!--Modal popups html-->
+		<div class="modal hide fade" id="about-modal">
+			<div class="modal-header">
+				  <a href="#" class="close">&times;</a>
+				  <h3>About</h3>
+			</div>
+			<div class="modal-body">
+				<p>Espresso is a web-frontend for the ESPRESSO logic minimization tool originally developed at IBM.</p>
+				<p>You can read more about it at the following links:</p>
+				<ul>
+					<li><a href="http://en.wikipedia.org/wiki/Espresso_heuristic_logic_minimizer">Wikipedia</a></li>
+					<li><a href="http://diamond.gem.valpo.edu/~dhart/ece110/espresso/tutorial.html">Tutorial</a></li>
+				</ul>
+			</div>
+			<div class="modal-footer">
+				  <a href="" class="btn primary">Close</a>
+			</div>
+		</div><!--end About modal-->		
 	</body>
 </html>
 
